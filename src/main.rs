59eb25948ea8586abcae01a70e8167cb33bdeddb
Collin Valley <collin.valley@gmail.com>
2019-04-10 20:54:00 -0700

    Add some more changes to my personal website.
    Really just messing around, this will be wiped clean later.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@
 #[macro_use] extern crate rocket;
 use rocket::response::NamedFile;
 use std::io;
+use std::path::{Path, PathBuf};
 
 #[get("/")]
 fn homepage() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
 }
 
+#[get("/<file..>")]
+fn files(file: PathBuf) -> io::Result<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file))
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![homepage]).launch();
+    rocket::ignite().mount("/", routes![homepage,files]).launch();
 }
