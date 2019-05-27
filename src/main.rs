ea14055cabf079ce5854d75ed67a63b0776a0f4d
Collin Valley <collin.valley@gmail.com>
2019-05-27 12:30:31 -0700

    update rocket server to work with Jekyll website

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@
 use rocket::response::NamedFile;
 use rocket::request::{self, Request, FromRequest};
 use std::io;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use rocket::Outcome;
 use rocket::http::Status;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Host {
     CollinValley,
     DawnCronin,
@@ -38,22 +38,34 @@ impl<'a, 'r> FromRequest<'a, 'r> for Host {
 fn known_host_homepage(host: Host) -> io::Result<NamedFile> {
     print!("{:?}", host);
     match host {
-        Host::CollinValley => { NamedFile::open("static/collinvalleyhome.html") },
-        Host::DawnCronin =>  { NamedFile::open("static/dawncroninhome.html") },
-        Host::Indeterminate => { NamedFile::open("static/collinvalleyhome.html") },
+        Host::CollinValley => { NamedFile::open("static/collinvalley/index.html") },
+        Host::DawnCronin =>  { NamedFile::open("static/dawncronin/dawncroninhome.html") },
+        Host::Indeterminate => { NamedFile::open("static/collinvalley/index.html") },
     }
 }
 
 #[get("/", rank=2)]
 fn unknown_host_homepage() -> io::Result<NamedFile> {
-    NamedFile::open("static/index.html")
+    NamedFile::open("static/collinvalley/index.html")
 }
 
-#[get("/<file..>")]
-fn files(file: PathBuf) -> io::Result<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file))
-}
+#[get("/<file..>", rank=1)]
+fn files(host: Host, file: PathBuf) -> io::Result<NamedFile> {
+    let mut path = PathBuf::new();
+    path.push("static");
+    match host {
+        Host::CollinValley => { path.push("collinvalley"); },
+        Host::DawnCronin => { path.push("dawncronin"); },
+        Host::Indeterminate => { path.push("collinvalley"); },
+    }
+    path.push(file);
 
+    if path.is_dir() {
+        path.push("index.html");
+    }
+    
+    NamedFile::open(path)
+}
 
 fn main() {
     rocket::ignite().mount("/", routes![known_host_homepage,
